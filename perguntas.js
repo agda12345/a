71771d28177f5eedca0b8349f6682fac8262cd2e
agda12345 <00001117734973sp@al.educacao.sp.gov.br>
2024-12-05 19:09:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'apenas um show',
+    'quem é o personagem que é um pássaro azul?',
+    'Mordecai'
+)
+
+criaCartao(
+    'apenas um show',
+    'quantas mãos o fantasmão tem?',
+    'apenas uma'
+)
+
+criaCartao(
+    'apenas um show',
+    'qual é a cor de pele do musculoso?',
+    'verde'
+)
+
+criaCartao(
+    'apenas um show',
+    'quem é a dupla protagonista da série?',
+    'Rigby e Mordecai'
+)
+
+criaCartao(
+    'aapenas um show',
+    'onde se passa as primeiras temporadas da série?',
+    'no parque'
+)
